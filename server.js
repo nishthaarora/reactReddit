59cb377751e9b9f73266ad2bfaa12c0e23693c34
Nishtha <nishtha.arora26@gmail.com>
2017-01-07 09:29:16 -0600

    server changes

--- server.js
+++ server.js
@@ -15,9 +15,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/create', publish);
 app.use('/posts', posts);
 
-
-mongoose.connect('mongodb://localhost/reddit');
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost/reddit' );
 const db = mongoose.connection;
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
 
 app.listen(PORT, () => {
 	console.log('server started on port: ', PORT);
